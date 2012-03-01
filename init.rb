8a4edfc0abce80beb4082a05e198e6c95897537b
Keith Humm <keith@spronkey.com>
2012-03-01 01:45:46 +0000

    Plugin up

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_wiki_h2 do
+  name 'Redmine Wiki H2 plugin'
+  author 'Keith Humm <spronkey>'
+  description 'Plugin to add H2 with wiki page title to the top of all wiki pages'
+  version '0.0.1'
+  url 'http://github.com/spronkey/redmine_wiki_h2'
+  author_url 'http://spronkey.com/'
+end
